6be69639c0ba291ba2a09bbf9f9a80ddcc460731
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-02-27 23:41:38 +0900

    Added preset for list and object
    Added link preset for list and multi list, also added preset for multi object linking.
    Changed some words in docstrings

--- attrLinker/presets.py
+++ attrLinker/presets.py
@@ -6,7 +6,7 @@ from .attrLinker import LinkManager
 
 def linkDictionary(targetClass, sourceVar, targetVar, sourceDictKey, default=None, enableSetter=False, doc='', **kw):
     '''
-    Link an attribute on an object to the source attribute's key, where the source attribute type is dictionary.
+    Link an attribute on an instance of targetClass to the source attribute's key, where the source attribute type is dictionary.
     Example: linkDictionary(Foo, 'dictionary', 'bar', 'bar_key', default='default_value')
     'Foo.bar' Gets value by 'Foo.dictionary.get('bar_key', 'default_value')'
 
@@ -42,12 +42,51 @@ def multiLinkDictionary(targetClass, sourceVar, linkMap={}, **kw):
     '''
     for sourceDictKey, targetVar in linkMap.items():
         linkDictionary(targetClass, sourceVar, targetVar, sourceDictKey, **kw)
-       
+
+
+def linkList(targetClass, sourceVar, targetVar, sourceIndex, enableSetter=False, doc='', **kw):
+    '''
+    Link an attribute on an instance of targetClass to source attribute's item on given index. 
+    Example: linkList(Foo, '_list_of_bars', 'first_bar', 0)
+    'Foo.first_bar' Gets its value by 'Foo._list_of_bars[0]'
+
+    Params
+    ------
+    targetClass:type is the class for the linking to be applied at
+    sourceVar:str is the source variable name of an instance of the targetClass
+    targetVar:str is the attribute name on the class to be linked to
+    sourceIndex:int is the target index on the source object
+    enableSetter:bool is basically, whether you want the targetVar attribute to have read-only access or full-access to the variable. Defaults to False(read-only)
+    doc:str is the documentation string for the property created
+    
+    Extra keyword argument passed, would be passed directly to manager.bind
+    '''
+    getterConverter = lambda _lst: _lst[sourceIndex]
+    setterOverrider = lambda linkedSelf, linkedVar, replacement: [linkedSelf.__getattribute__(linkedVar).pop(sourceIndex), linkedSelf.__getattribute__(linkedVar).insert(sourceIndex, replacement)]
+    manager = LinkManager._getDefault()
+    manager.bind(targetClass, sourceVar, targetVar, getterConverter, setterOverrider=setterOverrider, setupOptions={'enableSetter': enableSetter}, doc=doc, **kw)
+
+
+def multiLinkList(targetClass, sourceVar, linkMap={}, **kw):
+    '''
+    Calls linkList for every pair in linkMap.
+
+    Params
+    ------
+    targetClass:type is the class for the linking to be applied at
+    sourceVar:str is the source variable name of an instance of the targetClass, which must be of type list
+    linkMap:dict is the mapping for the linking, the mapping should be in the format as follows: {index_in_list:attribute_name_on_instance,...}
+
+    Extra keyword argument passed, would be passed directly to linkList
+    '''
+    for sourceListIndex, targetVar in linkMap.items():
+        linkList(targetClass, sourceVar, targetVar, sourceListIndex, **kw)
+
 
 def linkObject(targetClass, sourceVar, targetVar, sourceAttribute, enableSetter=False, doc='', **kw):
     '''
-    Link an attribute on an object to source attribute's attribute, where the source attribute type is Any.
-    Example: linkObject(Foo, 'obj', 'obj_attr1', 'attr_1', default='default_value')
+    Link an attribute on an instance of targetClass to source attribute's attribute, where the source attribute type is Any.
+    Example: linkObject(Foo, 'obj', 'obj_attr1', 'attr_1')
     'Foo.obj_attr1' Gets value by 'Foo.obj.attr_1'
 
     Params
@@ -56,7 +95,6 @@ def linkObject(targetClass, sourceVar, targetVar, sourceAttribute, enableSetter=
     sourceVar:str is the source variable name of an instance of the targetClass
     targetVar:str is the attribute name on the class to be linked to
     sourceAttribute:str is the attribute name on the source object
-    default:Any is the default return value for dictionary.get
     enableSetter:bool is basically, whether you want the targetVar attribute to have read-only access or full-access to the variable. Defaults to False(read-only)
     doc:str is the documentation string for the property created
     
@@ -67,3 +105,19 @@ def linkObject(targetClass, sourceVar, targetVar, sourceAttribute, enableSetter=
     setterOverrider = lambda linkedSelf, linkedVar, replacement: setattr(linkedSelf.__getattribute__(linkedVar), sourceAttribute, replacement)
     manager = LinkManager._getDefault()
     manager.bind(targetClass, sourceVar, targetVar, getterConverter, setterOverrider=setterOverrider, setupOptions={'enableSetter': enableSetter}, doc=doc, **kw)
+
+
+def multiLinkObject(targetClass, sourceVar, linkMap={}, **kw):
+    '''
+    Calls linkObject for every pair in linkMap.
+
+    Params
+    ------
+    targetClass:type is the class for the linking to be applied at
+    sourceVar:str is the source variable name of an instance of the targetClass
+    linkMap:dict is the mapping for the linking, the mapping should be in the format as follows: {attribute_on_source_object:attribute_name_on_instance,...}
+
+    Extra keyword argument passed, would be passed directly to linkList
+    '''
+    for sourceObjectAttr, targetVar in linkMap.items():
+        linkList(targetClass, sourceVar, targetVar, sourceObjectAttr, **kw)

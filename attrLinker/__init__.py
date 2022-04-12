50759d775071cbd9e6ac56eb5fca8dfc0cf66ce5
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-04-12 17:28:23 +0900

    Update __init__ imports from presets

--- attrLinker/__init__.py
+++ attrLinker/__init__.py
@@ -1,4 +1,4 @@
 from .attrLinker import Linker, LinkManager
-from .presets import linkDictionary, multiLinkDictionary, linkObject
+from .presets import linkDictionary, multiLinkDictionary, formattedTextFromDict, linkList, multiLinkList, linkObject, multiLinkObject
 from .exceptions import LinkerException, LinkerExists, LinkerNotFound, LinkerNotReady
 

d7939267f9df2f0fd8c35558490f6a7534873b2c
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-04-12 17:14:36 +0900

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,4 @@
-from attrLinker.presets import multiLinkDictionary
+from attrLinker.presets import multiLinkDictionary, formattedTextFromDict
 
 
 class User:
@@ -8,20 +8,22 @@ class User:
     def fetchData(self):
         self.userData = {'id':1, 'Name':'Foo', 'online_time':0, 'status': 'idle'}
         
-# linkMap pair => {source_key: destination_attribute, ...}
-linkMap = {'id':'ID', 'online_time':'onlineTime', 'status':'status', 'Name':'name'}
+# linkMap pair => {destination_attribute: source_key, ...}
+linkMap = {'ID':'id', 'name':'Name', 'onlineTime':'online_time', 'status':'status'}
 multiLinkDictionary(User, sourceVar='userData', linkMap=linkMap, enableSetter=True)
+formattedTextFromDict(User, 'userData', 'nameTag', "{Name}#{id}")
 # IMPORTANT: please do not link inside __init__. 
 
 testuser = User()
 testuser.fetchData()
 
-def testDictMap(obj, attrsMap):
-    for key, attr in attrsMap.items():
+def testDictMap(user, attrsMap):
+    for attr, key in attrsMap.items():
         try:
-            assert testuser.__getattribute__(attr) == testuser.userData.get(key)
-            print("Attribute={} Matches Key={}. Value={}".format(attr, key, testuser.__getattribute__(attr)))
+            assert user.__getattribute__(attr) == user.userData.get(key)
+            print("Attribute={} Matches Key={}. Value={}".format(attr, key, user.__getattribute__(attr)))
         except AssertionError as exc:
-            raise AssertionError('Attribute={} and Key={} Does not match.'.format(attr, key))
+            raise AssertionError('Attribute={} and Key={} Does not match. Exc={}'.format(attr, key, '{e}{e.args}'.format(e=exc)))
 
 testDictMap(testuser, linkMap)
+assert testuser.nameTag == "{Name}#{id}".format(**testuser.userData)

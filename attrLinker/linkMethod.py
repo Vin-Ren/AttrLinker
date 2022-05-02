f0447a9357856799a1198eeee1dd80c62b182228
vin-ren <65110648+vin-ren@users.noreply.github.com>
2022-05-02 13:28:11 +0900

    Added an enum for LinkMethods
    Added an enumeration for LinkMethods (QoL)
    
    This would be used for prepared links.

---
+++ attrLinker/linkMethod.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+from .presets import linkDictionary, multiLinkDictionary, linkList, multiLinkList, linkObject, multiLinkObject, formattedTextFromDict
+
+
+class LinkMethod(Enum):
+    DirectLink = None # A bypass to manager.bind
+    Dictionary = linkDictionary
+    MultiDictionary = multiLinkDictionary
+    List = linkList
+    MultiList = multiLinkList
+    Object = linkObject
+    MultiObject = multiLinkObject
+    FormattedText = formattedTextFromDict
+
+DirectLink = LinkMethod.DirectLink
+Dictionary = LinkMethod.Dictionary
+MultiDictionary = LinkMethod.MultiDictionary
+List = LinkMethod.List
+MultiList = LinkMethod.MultiList
+Object = LinkMethod.Object
+MultiObject = LinkMethod.MultiObject
+FormattedText = LinkMethod.FormattedText
